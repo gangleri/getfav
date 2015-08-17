2f93020d7e2ae4aaed57fb794cf8c6e4407a6b30
Alan Bradley <alan@gangleri.net>
2015-08-17 10:30:46 +0100

    correct install name

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -e
 
 { # ensure the entire script is downloaded
 
-	SCRIPT_URL='https://raw.githubusercontent.com/gangleri/getfav/master/getfav'
-	INSTALL_NAME='/usr/local/bin/getfav'
+	SCRIPT_URL='https://raw.githubusercontent.com/gangleri/getfav/master/grabfav'
+	INSTALL_NAME='/usr/local/bin/grabfav'
 	
 	curl  -s -L -o $INSTALL_NAME $SCRIPT_URL
 	chmod +x $INSTALL_NAME
